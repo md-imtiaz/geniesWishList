a8979ecfc324289b0191057b0dac8c9f0dca5180
jubo445 <juboraj335.ja@gmail.com>
2025-11-19 21:47:36 +0600

    actionBar added, navigate from start activity to dashboard activity, solve the error

--- app/src/main/java/com/shuvostechworld/genieswishlist/views/starter/StartFragment.kt
+++ app/src/main/java/com/shuvostechworld/genieswishlist/views/starter/StartFragment.kt
@@ -1,28 +1,27 @@
 package com.shuvostechworld.genieswishlist.views.starter
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.shuvostechworld.genieswishlist.R
 import com.shuvostechworld.genieswishlist.base.BaseFragment
 import com.shuvostechworld.genieswishlist.databinding.FragmentStartBinding
+import com.shuvostechworld.genieswishlist.views.dashboard.seller.SellerDashboard
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class StartFragment : BaseFragment<FragmentStartBinding> (FragmentStartBinding::inflate){
+
+
     override fun setListener() {
         setupAutoLogin()
         with(binding) {
-            binding.btnLogin.setOnClickListener {
+            btnLogin.setOnClickListener {
                 findNavController().navigate(R.id.action_startFragment_to_loginFragment)
             }
-            binding.btnRegister.setOnClickListener {
+            btnRegister.setOnClickListener {
                 findNavController().navigate(R.id.action_startFragment_to_registerFragment)
             }
         }
@@ -31,7 +30,8 @@ class StartFragment : BaseFragment<FragmentStartBinding> (FragmentStartBinding::
 
     private fun setupAutoLogin () {
         FirebaseAuth.getInstance().currentUser?.let {
-            findNavController().navigate(R.id.action_startFragment_to_dashboardFragment)
+            startActivity(Intent(requireContext(), SellerDashboard::class.java))
+            requireActivity().finish()
         }
     }
 
